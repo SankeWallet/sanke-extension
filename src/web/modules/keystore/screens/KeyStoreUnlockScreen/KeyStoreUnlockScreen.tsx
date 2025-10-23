a55a37401fdacf8d994ef813ee43bb30c01cc9b6
SankeWallet <gjschwenke333@live.com>
2025-10-23 05:54:22 +0000

    feat: Add privacy mode toggle

--- src/web/modules/keystore/screens/KeyStoreUnlockScreen/KeyStoreUnlockScreen.tsx
+++ src/web/modules/keystore/screens/KeyStoreUnlockScreen/KeyStoreUnlockScreen.tsx
@@ -27,7 +27,7 @@ import useBackgroundService from '@web/hooks/useBackgroundService'
 import useKeystoreControllerState from '@web/hooks/useKeystoreControllerState'
 import { getUiType } from '@web/utils/uiType'
 
-import KohakuLogo from '@common/components/HokahuLogo'
+import SankeLogo from '@common/components/SankeLogo'
 import getStyles from './styles'
 
 // const FOOTER_BUTTON_HIT_SLOP = { top: 10, bottom: 15 }
@@ -173,7 +173,7 @@ const KeyStoreUnlockScreen = () => {
               </View>
             )}
             <View style={[flexbox.flex1, flexbox.alignCenter, flexbox.justifyCenter]}>
-              <KohakuLogo width={300} height={height < 550 && !isPopup ? 90 : 140} />
+              <SankeLogo width={300} height={height < 550 && !isPopup ? 90 : 140} />
             </View>
             <View>
               <Text
